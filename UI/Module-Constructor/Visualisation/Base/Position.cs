a89305541231bf93e3f5631c8fc6bdfd974cef5a
Денис <dultsevdv@gmail.com>
2022-02-17 00:44:46 +0500

    Модели для визуализации

---
+++ UI/Module-Constructor/Visualisation/Base/Position.cs
@@ -0,0 +1,11 @@
+﻿namespace Module_Constructor.Visualisation.Base;
+
+/// <summary> Позиция детали или секции (нулевая точка - внизу слева сзади) </summary>
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public override string ToString() => $"X={X} Y={Y} Z={Z}";
+}
---
+++ UI/Module-Constructor/Visualisation/PanelViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using Module_Constructor.Visualisation.Base;
+
+namespace Module_Constructor.Visualisation
+{
+    public class PanelViewModel
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Depth { get; set; }
+
+        public Position Position { get; set; }
+
+        public Orientation Orientation { get; set; }
+
+    }
+}
