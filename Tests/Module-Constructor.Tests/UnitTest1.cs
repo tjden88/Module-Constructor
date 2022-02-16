dc55a8631be1ccb04220ea6ec79d368f1a527707
Денис <dultsevdv@gmail.com>
2022-02-16 22:41:34 +0500

    Организация структуры решения

---
+++ Tests/Module-Constructor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Module_Constructor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
