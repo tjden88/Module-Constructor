eae0be3c2f943be59d5d5233f15448158f32b9cb
Денис <dultsevdv@gmail.com>
2022-02-17 21:49:52 +0500

    Заготовка сервиса построителя модулей

--- UI/Module-Constructor/Services/3DVisualizer.cs
+++ UI/Module-Constructor/Services/3DVisualizer.cs
@@ -8,16 +8,20 @@ using HelixToolkit.Wpf;
 using Microsoft.Extensions.Logging;
 using Module_Constructor.Models;
 using Module_Constructor.Services.Interfaces;
-using Module_Constructor.Visualisation;
 
 namespace Module_Constructor.Services
 {
     public class Visualizer : I3DVisualizer
     {
         private readonly ILogger _Logger;
+        private readonly IModuleBuilder _ModuleBuilder;
 
 
-        public Visualizer(ILogger<Visualizer> Logger) => _Logger = Logger;
+        public Visualizer(ILogger<Visualizer> Logger, IModuleBuilder ModuleBuilder)
+        {
+            _Logger = Logger;
+            _ModuleBuilder = ModuleBuilder;
+        }
 
 
         public Model3D CreateModel(Module Module, Panel SelectedPanel)
@@ -34,26 +38,12 @@ namespace Module_Constructor.Services
             var insideMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
 
 
-            var panels = new List<PanelViewModel>();
 
-            foreach (var modulePart in Module.Panels)
+            foreach (var panelModel in _ModuleBuilder.BuildPanels(Module))
             {
-                var isSelected = SelectedPanel?.Equals(modulePart);
-
-                var panelModel = BuildViewModel((Panel)modulePart, Module.Width, Module.Height, Module.Depth);
-
-                ExcludeCollisions(panelModel, panels, Module.Width, Module.Height, Module.Depth);
-
-                if (panelModel.HasErrors)
-                {
-                    _Logger.LogWarning("Ошибка построения детали {0}", panelModel.Panel.Name);
-                    continue;
-                }
-
-                panels.Add(panelModel);
+                var isSelected = SelectedPanel?.Equals(panelModel.Panel);
 
                 var meshBuilder = new MeshBuilder(false, false);
-
 
                 var locationPoint = new Point3D(panelModel.Position.Z, panelModel.Position.X, panelModel.Position.Y);
                 var size = new Size3D(panelModel.Depth, panelModel.Width, panelModel.Height);
@@ -74,196 +64,5 @@ namespace Module_Constructor.Services
             return modelGroup;
         }
 
-
-        #region Private Methods
-
-        // Построить модель из детали
-        private PanelViewModel BuildViewModel(Panel panel, int AreaWidth, int AreaHeight, int AreaDepth)
-        {
-            var viewModel = new PanelViewModel()
-            {
-                Panel = panel
-            };
-
-            // Установить размеры и положение в зависимости от ориентации
-            switch (panel.Orientation)
-            {
-                case Panel.PanelOrientation.Horizontal:
-                    viewModel.Width = panel.FixedLenght ?? GetSize(AreaWidth, panel.LeftMargin, panel.RightMargin);
-                    viewModel.Height = panel.Thickness;
-                    viewModel.Depth = panel.FixedWidth ?? GetSize(AreaDepth, panel.FrontMargin, panel.BackMargin);
-
-                    viewModel.IsFixedWidth = panel.FixedLenght != null;
-                    viewModel.IsFixedHeight = true;
-                    viewModel.IsFixedDepth = panel.FixedWidth != null;
-
-                    break;
-                case Panel.PanelOrientation.Vertical:
-                    viewModel.Width = panel.Thickness;
-                    viewModel.Height = panel.FixedLenght ?? GetSize(AreaHeight, panel.BottomMargin, panel.TopMargin);
-                    viewModel.Depth = panel.FixedWidth ?? GetSize(AreaDepth, panel.FrontMargin, panel.BackMargin);
-
-                    viewModel.IsFixedWidth = true;
-                    viewModel.IsFixedHeight = panel.FixedLenght != null;
-                    viewModel.IsFixedDepth = panel.FixedWidth != null;
-                    break;
-                case Panel.PanelOrientation.Frontal:
-                    viewModel.Width = panel.FixedLenght ?? GetSize(AreaWidth, panel.LeftMargin, panel.RightMargin);
-                    viewModel.Height = panel.FixedWidth ?? GetSize(AreaHeight, panel.BottomMargin, panel.TopMargin);
-                    viewModel.Depth = panel.Thickness;
-
-                    viewModel.IsFixedWidth = panel.FixedLenght != null;
-                    viewModel.IsFixedHeight = panel.FixedWidth != null;
-                    viewModel.IsFixedDepth = true;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            //// Установить смещения в соответствии с отступами
-            viewModel.Position.X = GetOffset(AreaWidth, viewModel.Width, panel.LeftMargin, panel.RightMargin);
-            viewModel.Position.Y = GetOffset(AreaHeight, viewModel.Height, panel.BottomMargin, panel.TopMargin);
-            viewModel.Position.Z = GetOffset(AreaDepth, viewModel.Depth, panel.BackMargin, panel.FrontMargin);
-
-            return viewModel;
-        }
-
-
-        // Получить размер детали
-        private int GetSize(int Bound, int? FirstMargin, int? SecondMargin)
-        {
-            if (FirstMargin.HasValue)
-            {
-                Bound -= FirstMargin.Value;
-            }
-
-            if (SecondMargin.HasValue)
-            {
-                Bound -= SecondMargin.Value;
-            }
-            return Bound;
-        }
-
-
-        // Получить смещение детали отностиельно начала координат
-        private int GetOffset(int Bound, int DetalSize, int? FirstMargin, int? SecondMargin)
-        {
-            if (FirstMargin.HasValue)
-            {
-                return FirstMargin.Value;
-            }
-
-            if (SecondMargin.HasValue)
-            {
-                return Bound - DetalSize - SecondMargin.Value;
-            }
-
-            return (Bound - DetalSize) / 2;
-        }
-
-
-        // Устранить пересечения
-        private void ExcludeCollisions(PanelViewModel current, ICollection<PanelViewModel> previousModels, int AreaWidth, int AreaHeight, int AreaDepth)
-        {
-
-            var leftOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Left && current.Panel.Anchor != Panel.PanelAnchor.Right)
-                .Where(p => CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height)
-                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
-                .Select(p => p.Position.X + p.Width)
-                .DefaultIfEmpty()
-                .Max();
-
-
-            var rightOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Right && current.Panel.Anchor != Panel.PanelAnchor.Left)
-                .Where(p => CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height)
-                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
-                .Select(p => AreaWidth - p.Position.X)
-                .DefaultIfEmpty()
-                .Max();
-
-            if (current.IsFixedWidth)
-            {
-                if (current.Panel.LeftMargin is { }) current.Position.X += leftOffset;
-                if (current.Panel.RightMargin is { }) current.Position.X -= rightOffset;
-            }
-            else
-            {
-                current.Position.X += leftOffset;
-                current.Width -= rightOffset + leftOffset;
-            }
-
-
-            var bottomOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Bottom && current.Panel.Anchor != Panel.PanelAnchor.Top)
-                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
-                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
-                .Select(p => p.Position.Y + p.Height)
-                .DefaultIfEmpty()
-                .Max();
-
-
-
-            var topOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Top && current.Panel.Anchor != Panel.PanelAnchor.Bottom)
-                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
-                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
-                .Select(p => AreaHeight - p.Position.Y)
-                .DefaultIfEmpty()
-                .Max();
-
-            if (current.IsFixedHeight)
-            {
-                if (current.Panel.BottomMargin is { }) current.Position.Y += bottomOffset;
-                if (current.Panel.TopMargin is { }) current.Position.Y -= topOffset;
-            }
-            else
-            {
-                current.Position.Y += bottomOffset;
-                current.Height -= topOffset + bottomOffset;
-            }
-
-            var backOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Back && current.Panel.Anchor != Panel.PanelAnchor.Front)
-                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
-                            && CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height))
-                .Select(p => p.Position.Z + p.Depth)
-                .DefaultIfEmpty()
-                .Max();
-
-
-
-            var frontOffset = previousModels
-                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Front && current.Panel.Anchor != Panel.PanelAnchor.Back)
-                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
-                            && CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height))
-                .Select(p => AreaDepth - p.Position.Z)
-                .DefaultIfEmpty()
-                .Max();
-
-            if (current.IsFixedDepth)
-            {
-                if (current.Panel.BackMargin is { }) current.Position.Z += backOffset;
-                if (current.Panel.FrontMargin is { }) current.Position.Z -= frontOffset;
-            }
-            else
-            {
-                current.Position.Z += backOffset;
-                current.Depth -= frontOffset + backOffset;
-            }
-
-        }
-
-
-        // Определить пересечения по отрезкам
-        private bool CheckCollision(int point1, int lenght1, int point2, int lenght2)
-        {
-            var finish1 = point1 + lenght1;
-            var finish2 = point2 + lenght2;
-
-            return finish2 > point1 && point2 < finish1;
-        }
-
-        #endregion
     }
 }
--- UI/Module-Constructor/Services/ModuleBuilder.cs
+++ UI/Module-Constructor/Services/ModuleBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
 using Module_Constructor.Models;
 using Module_Constructor.Services.Interfaces;
 using Module_Constructor.Visualisation;
@@ -8,9 +10,34 @@ namespace Module_Constructor.Services
 {
     public class ModuleBuilder : IModuleBuilder
     {
+        private readonly ILogger<ModuleBuilder> _Logger;
+
+        public ModuleBuilder(ILogger<ModuleBuilder> Logger)
+        {
+            _Logger = Logger;
+        }
+
         public IEnumerable<PanelViewModel> BuildPanels(Module module)
         {
-            throw new NotImplementedException();
+            var panels = new List<PanelViewModel>();
+
+            foreach (var modulePart in module.Panels)
+            {
+
+                var panelModel = BuildViewModel((Panel)modulePart, module.Width, module.Height, module.Depth);
+
+                ExcludeCollisions(panelModel, panels, module.Width, module.Height, module.Depth);
+
+                if (panelModel.HasErrors)
+                {
+                    _Logger.LogWarning("Ошибка построения детали {0}", panelModel.Panel.Name);
+                    continue;
+                }
+
+                panels.Add(panelModel);
+            }
+
+            return panels;
         }
 
         public PanelViewModel BuildPanel(Module module)
@@ -32,5 +59,198 @@ namespace Module_Constructor.Services
         {
             throw new NotImplementedException();
         }
+
+
+        #region Private Methods
+
+        // Построить модель из детали
+        private PanelViewModel BuildViewModel(Panel panel, int AreaWidth, int AreaHeight, int AreaDepth)
+        {
+            var viewModel = new PanelViewModel()
+            {
+                Panel = panel
+            };
+
+            // Установить размеры и положение в зависимости от ориентации
+            switch (panel.Orientation)
+            {
+                case Panel.PanelOrientation.Horizontal:
+                    viewModel.Width = panel.FixedLenght ?? GetSize(AreaWidth, panel.LeftMargin, panel.RightMargin);
+                    viewModel.Height = panel.Thickness;
+                    viewModel.Depth = panel.FixedWidth ?? GetSize(AreaDepth, panel.FrontMargin, panel.BackMargin);
+
+                    viewModel.IsFixedWidth = panel.FixedLenght != null;
+                    viewModel.IsFixedHeight = true;
+                    viewModel.IsFixedDepth = panel.FixedWidth != null;
+
+                    break;
+                case Panel.PanelOrientation.Vertical:
+                    viewModel.Width = panel.Thickness;
+                    viewModel.Height = panel.FixedLenght ?? GetSize(AreaHeight, panel.BottomMargin, panel.TopMargin);
+                    viewModel.Depth = panel.FixedWidth ?? GetSize(AreaDepth, panel.FrontMargin, panel.BackMargin);
+
+                    viewModel.IsFixedWidth = true;
+                    viewModel.IsFixedHeight = panel.FixedLenght != null;
+                    viewModel.IsFixedDepth = panel.FixedWidth != null;
+                    break;
+                case Panel.PanelOrientation.Frontal:
+                    viewModel.Width = panel.FixedLenght ?? GetSize(AreaWidth, panel.LeftMargin, panel.RightMargin);
+                    viewModel.Height = panel.FixedWidth ?? GetSize(AreaHeight, panel.BottomMargin, panel.TopMargin);
+                    viewModel.Depth = panel.Thickness;
+
+                    viewModel.IsFixedWidth = panel.FixedLenght != null;
+                    viewModel.IsFixedHeight = panel.FixedWidth != null;
+                    viewModel.IsFixedDepth = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            //// Установить смещения в соответствии с отступами
+            viewModel.Position.X = GetOffset(AreaWidth, viewModel.Width, panel.LeftMargin, panel.RightMargin);
+            viewModel.Position.Y = GetOffset(AreaHeight, viewModel.Height, panel.BottomMargin, panel.TopMargin);
+            viewModel.Position.Z = GetOffset(AreaDepth, viewModel.Depth, panel.BackMargin, panel.FrontMargin);
+
+            return viewModel;
+        }
+
+
+        // Получить размер детали
+        private int GetSize(int Bound, int? FirstMargin, int? SecondMargin)
+        {
+            if (FirstMargin.HasValue)
+            {
+                Bound -= FirstMargin.Value;
+            }
+
+            if (SecondMargin.HasValue)
+            {
+                Bound -= SecondMargin.Value;
+            }
+            return Bound;
+        }
+
+
+        // Получить смещение детали отностиельно начала координат
+        private int GetOffset(int Bound, int DetalSize, int? FirstMargin, int? SecondMargin)
+        {
+            if (FirstMargin.HasValue)
+            {
+                return FirstMargin.Value;
+            }
+
+            if (SecondMargin.HasValue)
+            {
+                return Bound - DetalSize - SecondMargin.Value;
+            }
+
+            return (Bound - DetalSize) / 2;
+        }
+
+
+        // Устранить пересечения
+        private void ExcludeCollisions(PanelViewModel current, ICollection<PanelViewModel> previousModels, int AreaWidth, int AreaHeight, int AreaDepth)
+        {
+
+            var leftOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Left && current.Panel.Anchor != Panel.PanelAnchor.Right)
+                .Where(p => CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height)
+                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
+                .Select(p => p.Position.X + p.Width)
+                .DefaultIfEmpty()
+                .Max();
+
+
+            var rightOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Right && current.Panel.Anchor != Panel.PanelAnchor.Left)
+                .Where(p => CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height)
+                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
+                .Select(p => AreaWidth - p.Position.X)
+                .DefaultIfEmpty()
+                .Max();
+
+            if (current.IsFixedWidth)
+            {
+                if (current.Panel.LeftMargin is { }) current.Position.X += leftOffset;
+                if (current.Panel.RightMargin is { }) current.Position.X -= rightOffset;
+            }
+            else
+            {
+                current.Position.X += leftOffset;
+                current.Width -= rightOffset + leftOffset;
+            }
+
+
+            var bottomOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Bottom && current.Panel.Anchor != Panel.PanelAnchor.Top)
+                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
+                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
+                .Select(p => p.Position.Y + p.Height)
+                .DefaultIfEmpty()
+                .Max();
+
+
+
+            var topOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Top && current.Panel.Anchor != Panel.PanelAnchor.Bottom)
+                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
+                            && CheckCollision(current.Position.Z, current.Depth, p.Position.Z, p.Depth))
+                .Select(p => AreaHeight - p.Position.Y)
+                .DefaultIfEmpty()
+                .Max();
+
+            if (current.IsFixedHeight)
+            {
+                if (current.Panel.BottomMargin is { }) current.Position.Y += bottomOffset;
+                if (current.Panel.TopMargin is { }) current.Position.Y -= topOffset;
+            }
+            else
+            {
+                current.Position.Y += bottomOffset;
+                current.Height -= topOffset + bottomOffset;
+            }
+
+            var backOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Back && current.Panel.Anchor != Panel.PanelAnchor.Front)
+                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
+                            && CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height))
+                .Select(p => p.Position.Z + p.Depth)
+                .DefaultIfEmpty()
+                .Max();
+
+
+
+            var frontOffset = previousModels
+                .Where(p => p.Panel.Anchor == Panel.PanelAnchor.Front && current.Panel.Anchor != Panel.PanelAnchor.Back)
+                .Where(p => CheckCollision(current.Position.X, current.Width, p.Position.X, p.Width)
+                            && CheckCollision(current.Position.Y, current.Height, p.Position.Y, p.Height))
+                .Select(p => AreaDepth - p.Position.Z)
+                .DefaultIfEmpty()
+                .Max();
+
+            if (current.IsFixedDepth)
+            {
+                if (current.Panel.BackMargin is { }) current.Position.Z += backOffset;
+                if (current.Panel.FrontMargin is { }) current.Position.Z -= frontOffset;
+            }
+            else
+            {
+                current.Position.Z += backOffset;
+                current.Depth -= frontOffset + backOffset;
+            }
+
+        }
+
+
+        // Определить пересечения по отрезкам
+        private bool CheckCollision(int point1, int lenght1, int point2, int lenght2)
+        {
+            var finish1 = point1 + lenght1;
+            var finish2 = point2 + lenght2;
+
+            return finish2 > point1 && point2 < finish1;
+        }
+
+        #endregion
+
     }
 }
